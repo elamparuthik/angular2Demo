0668a9ba48c1bc568ac2a099c0944098e73886c0
elamparuthik <elamparuthik@gmail.com>
2017-09-11 13:17:13 +0530

    after
    after

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,6 @@
 import 'rxjs/add/operator/switchMap';
 import { Component, OnInit }        from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';//ParamMap
 import { Location }                 from '@angular/common';
 
 import { Hero }        from '../model/hero';
@@ -21,9 +21,9 @@ export class HeroDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap
+    /*this.route.paramMap
       .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
-      .subscribe(hero => this.hero = hero);
+      .subscribe(hero => this.hero = hero);*/
   }
 
   save(): void {
